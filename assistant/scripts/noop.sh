96e63ef0068a69456e33f05ca905b0157d7cf315
Piotr Skowronek <piotr@skowro.net>
2020-06-08 00:42:34 +0200

    Trying to make xterm launch faster on RPi :/

--- assistant/scripts/noop.sh
+++ assistant/scripts/noop.sh
@@ -6,6 +6,14 @@ export DISPLAY=:0.0
 
 brighten_display
 TO_KILL=$(pgrep -f "term.*TAG=assistant")
-lxterm -fullscreen -fa 'Monospace' -fs 25 +sb -bc -e "TAG=assistant; echo 'Didnt get that. Call me again.' | pv -qL 8; sleep 2s" &
+
+# using fn and bitmap fonts as they terminal starts quicker (2s vs 3.5s). TTF can be loaded by using: -fa 'Monospace' -fs 25
+# list of mono fonts can be found by executing this command: xlsfonts -fn '*-*-*-*-*-*-*-*-*-*-*-m*'
+xterm -class UXTerm -title HAQnR -fn "-adobe-courier-medium-r-normal--25-180-100-100-m-150-iso8859-1" \
+      -u8 -fullscreen -u8 +wf +sb -bc -baudrate 230400 +s -e "TAG=assistant; echo \"Didn't get that. Call me again!\" | pv -qL 25; sleep 2s" &
+
+# One can also use lxterm which calls xterm:
+#lxterm -fullscreen -fa 'Monospace' -fs 25 +sb -bc -e "TAG=assistant; echo 'Didnt get that. Call me again.' | pv -qL 8; sleep 2s" &
+
 kill $TO_KILL
 espeak -v+m2 -s160 -a30 -p40 "Didn't get that. Speak my name and try again."
--- assistant/scripts/weather_tomorrow.sh
+++ assistant/scripts/weather_tomorrow.sh
@@ -5,7 +5,15 @@ source ./_helpers.sh
 export DISPLAY=:0.0
 
 brighten_display
+
+# Using wttr.in to display ASCII weather.
 # wttr.in is a cool thing! Check it out here https://github.com/chubin/wttr.in, more options can be seen this way: curl http://wttr.in/:help
-lxterm -fullscreen -fa 'Monospace' -fs 8 +sb -bc -e "curl wttr.in/?Ftnqp1; sleep 15s"
+
+# Using TTF fonts (-fa & -fs) instead of bitmap (-fn) to better fit into stream
+xterm -class UXTerm -title HAQnR -fa 'Monospace' -fs 8 \
+      -u8 -fullscreen -u8 +wf +sb -bc -baudrate 230400 +s -e "TAG=assistant; curl wttr.in/?Ftnqp1; sleep 15s" &
+
+# One can also use lxterm which calls xterm:
+#lxterm -fullscreen -fa 'Monospace' -fs 8 +sb -bc -e "curl wttr.in/?Ftnqp1; sleep 15s"
 
 
